20367ec6a1074a729195ff42d690906cb507124c
Gustavo Landfried <gustavolandfried@gmail.com>
2021-05-07 11:00:51 -0300

    example: details

--- example/example.py
+++ example/example.py
@@ -2,14 +2,15 @@ import sys
 sys.path.append('..')
 from src import *
 import timeit
-import trueskill as ts
 
-# Code 1
+print("Code 1")
 mu = 0.0; sigma = 6.0; beta = 1.0; gamma = 0.0
+
+print("Code 2")
 p1 = Player(Gaussian(mu, sigma), beta, gamma); p2 = Player(Gaussian(mu, sigma), beta, gamma)
 p3 = Player(Gaussian(mu, sigma), beta, gamma); p4 = Player(Gaussian(mu, sigma), beta, gamma)
 
-# Code 2
+print("Code 3")
 team_a = [ p1, p2 ]
 team_b = [ p3, p4 ]
 teams = [team_a, team_b]
@@ -17,18 +18,18 @@ g = Game(teams)
 time_tt = timeit.timeit(lambda: Game(teams).posteriors, number=1000)/1000
 # g = Game(teams, [0,0])
 
-# Code 3
+print("Code 4")
 lhs = g.likelihoods
 ev = g.evidence
 ev = round(ev, 3)
 print(ev)
 
-# Code 4
+print("Code 5")
 pos = g.posteriors()
 print(pos[0][0])
 print(lhs[0][0] * p1.prior)
 
-# Code 5
+print("Code 6")
 ta = [p1]
 tb = [p2, p3]
 tc = [p4]
@@ -37,30 +38,35 @@ result = [1, 0, 0]
 g = Game(teams, result, p_draw=0.25)
 time_tt = timeit.timeit(lambda: Game(teams, result, p_draw=0.25).posteriors, number=1000)/1000
 
-# Code 6
+print("Code 7")
 c1 = [["a"],["b"]]
 c2 = [["b"],["c"]]
 c3 = [["c"],["a"]]
 composition = [c1, c2, c3]
 h = History(composition)
 time_tt = timeit.timeit(lambda: History(composition), number=1000)/1000
-h.convergence()
-time_tt = timeit.timeit(lambda: h.convergence(iterations=1, verbose=False), number=10)/10
 
-# Code 7
+print("Code 8")
 lc = h.learning_curves()
 print(lc["a"])
 print(lc["b"])
 
+print("Code 9")
+h.convergence()
+time_tt = timeit.timeit(lambda: h.convergence(iterations=1, verbose=False), number=10)/10
+lc = h.learning_curves()
+print(lc["a"])
+print(lc["b"])
 
 ###
+print("Code 10")
 import math; from numpy.random import normal, seed; seed(99); N=1000
 def skill(experience, middle, maximum, slope):
     return maximum/(1+math.exp(slope*(-experience+middle)))
 
 target = [skill(i, 500, 2, 0.0075) for i in range(N)]
 mus = []
-for _ in range(33):
+for _ in range(1):
     opponents = normal(target,0.5)
     composition = [[["a"], [str(i)]] for i in range(N)]
     results = [  [1.,0.] if normal(target[i]) > normal(opponents[i]) else [0.,1.] for i in range(N)]
@@ -74,30 +80,15 @@ for _ in range(33):
 
 mus.append(target)
 
-for m in mus:
-    plt.plot(m, "--")
-
-plt.plot(target, color="black")
-plt.show()
-
-#import matplotlib.pyplot as plt
-#plt.plot(opponents)
-#plt.show()
-#plt.plot(mu_05)
-
+print("Skill evolution data: output/logistic.csv")
 import pandas as pd
-df = pd.DataFrame({"true" : agent, "mu" : mu, "sigma" : sigma})
+df = pd.DataFrame({"true" : target, "mu" : mu, "sigma" : sigma})
 df.to_csv("output/logisitc.csv", index=False)
-
 
 df = pd.DataFrame(mus)
 df.to_csv("output/logisitcs_mu.csv", index=False)
 
-
-#plt.plot(mu)
-#plt.plot(agent)
-#plt.show()
-
+print("Time analysis")
 import time
 start = time.time()
 h = History(composition, results, times, priors, mu=2.0, gamma=0.015)
@@ -108,17 +99,17 @@ h.convergence(iterations=1)
 end = time.time()
 end - start
 
-time_tt = timeit.timeit(lambda: History(composition, results, times, priors, mu=2.0, gamma=0.015), number=10)/10
-time_tt = timeit.timeit(lambda: h.convergence(iterations=1), number=10)/10
+#time_tt = timeit.timeit(lambda: History(composition, results, times, priors, mu=2.0, gamma=0.015), number=10)/10
+#time_tt = timeit.timeit(lambda: h.convergence(iterations=1), number=10)/10
 
 
+#import trueskill as ts
+#env = ts.TrueSkill(mu = mu, sigma = sigma, beta = beta, tau= gamma, draw_probability=0.0)
+#r1 = env.Rating(); r2 = env.Rating(); r3 = env.Rating(); r4 = env.Rating()
+#time_tt = timeit.timeit(lambda: env.rate([[r1,r2],[r3,r4]]), number=10000)/10000
 
-env = ts.TrueSkill(mu = mu, sigma = sigma, beta = beta, tau= gamma, draw_probability=0.0)
-r1 = env.Rating(); r2 = env.Rating(); r3 = env.Rating(); r4 = env.Rating()
-time_tt = timeit.timeit(lambda: env.rate([[r1,r2],[r3,r4]]), number=10000)/10000
-
-env = ts.TrueSkill(mu = mu, sigma = sigma, beta = beta, tau= gamma, draw_probability=0.25)
-r1 = env.Rating(); r2 = env.Rating(); r3 = env.Rating(); r4 = env.Rating()
-time_tt = timeit.timeit(lambda: env.rate([[r1],[r2,r3],[r4]],[0,1,1]), number=10000)/10000
+#env = ts.TrueSkill(mu = mu, sigma = sigma, beta = beta, tau= gamma, draw_probability=0.25)
+#r1 = env.Rating(); r2 = env.Rating(); r3 = env.Rating(); r4 = env.Rating()
+#time_tt = timeit.timeit(lambda: env.rate([[r1],[r2,r3],[r4]],[0,1,1]), number=10000)/10000
  
 
